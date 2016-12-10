7e74e0f7d7f4818b74762357a1eedf11a95b92c0
Benoit Paquet <benoxoft@gmail.com>
2016-12-10 14:29:26 -0500

    Day 8

---
+++ day8/day8.py
@@ -0,0 +1,205 @@
+data = """rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 6
+rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 5
+rect 2x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 4
+rect 1x1
+rotate row y=0 by 3
+rect 2x1
+rotate row y=0 by 7
+rect 3x1
+rotate row y=0 by 3
+rect 1x1
+rotate row y=0 by 3
+rect 1x2
+rotate row y=1 by 13
+rotate column x=0 by 1
+rect 2x1
+rotate row y=0 by 5
+rotate column x=0 by 1
+rect 3x1
+rotate row y=0 by 18
+rotate column x=13 by 1
+rotate column x=7 by 2
+rotate column x=2 by 3
+rotate column x=0 by 1
+rect 17x1
+rotate row y=3 by 13
+rotate row y=1 by 37
+rotate row y=0 by 11
+rotate column x=7 by 1
+rotate column x=6 by 1
+rotate column x=4 by 1
+rotate column x=0 by 1
+rect 10x1
+rotate row y=2 by 37
+rotate column x=19 by 2
+rotate column x=9 by 2
+rotate row y=3 by 5
+rotate row y=2 by 1
+rotate row y=1 by 4
+rotate row y=0 by 4
+rect 1x4
+rotate column x=25 by 3
+rotate row y=3 by 5
+rotate row y=2 by 2
+rotate row y=1 by 1
+rotate row y=0 by 1
+rect 1x5
+rotate row y=2 by 10
+rotate column x=39 by 1
+rotate column x=35 by 1
+rotate column x=29 by 1
+rotate column x=19 by 1
+rotate column x=7 by 2
+rotate row y=4 by 22
+rotate row y=3 by 5
+rotate row y=1 by 21
+rotate row y=0 by 10
+rotate column x=2 by 2
+rotate column x=0 by 2
+rect 4x2
+rotate column x=46 by 2
+rotate column x=44 by 2
+rotate column x=42 by 1
+rotate column x=41 by 1
+rotate column x=40 by 2
+rotate column x=38 by 2
+rotate column x=37 by 3
+rotate column x=35 by 1
+rotate column x=33 by 2
+rotate column x=32 by 1
+rotate column x=31 by 2
+rotate column x=30 by 1
+rotate column x=28 by 1
+rotate column x=27 by 3
+rotate column x=26 by 1
+rotate column x=23 by 2
+rotate column x=22 by 1
+rotate column x=21 by 1
+rotate column x=20 by 1
+rotate column x=19 by 1
+rotate column x=18 by 2
+rotate column x=16 by 2
+rotate column x=15 by 1
+rotate column x=13 by 1
+rotate column x=12 by 1
+rotate column x=11 by 1
+rotate column x=10 by 1
+rotate column x=7 by 1
+rotate column x=6 by 1
+rotate column x=5 by 1
+rotate column x=3 by 2
+rotate column x=2 by 1
+rotate column x=1 by 1
+rotate column x=0 by 1
+rect 49x1
+rotate row y=2 by 34
+rotate column x=44 by 1
+rotate column x=40 by 2
+rotate column x=39 by 1
+rotate column x=35 by 4
+rotate column x=34 by 1
+rotate column x=30 by 4
+rotate column x=29 by 1
+rotate column x=24 by 1
+rotate column x=15 by 4
+rotate column x=14 by 1
+rotate column x=13 by 3
+rotate column x=10 by 4
+rotate column x=9 by 1
+rotate column x=5 by 4
+rotate column x=4 by 3
+rotate row y=5 by 20
+rotate row y=4 by 20
+rotate row y=3 by 48
+rotate row y=2 by 20
+rotate row y=1 by 41
+rotate column x=47 by 5
+rotate column x=46 by 5
+rotate column x=45 by 4
+rotate column x=43 by 5
+rotate column x=41 by 5
+rotate column x=33 by 1
+rotate column x=32 by 3
+rotate column x=23 by 5
+rotate column x=22 by 1
+rotate column x=21 by 2
+rotate column x=18 by 2
+rotate column x=17 by 3
+rotate column x=16 by 2
+rotate column x=13 by 5
+rotate column x=12 by 5
+rotate column x=11 by 5
+rotate column x=3 by 5
+rotate column x=2 by 5
+rotate column x=1 by 5""".split("\n")
+
+example = """rect 3x2
+rotate column x=1 by 1
+rotate row y=0 by 4
+rotate column x=1 by 1""".split("\n")
+
+
+import numpy as np
+np.set_printoptions(threshold=np.nan, linewidth=220, formatter={'all': lambda x: ' ' if x == 0 else '#'})
+
+
+def rect(line: str, display: np.array) -> np.array:
+    y, x = line.split(" ")[1].split("x")
+    display[:int(x), :int(y)] = 1
+    return display
+
+
+def rotate_row(line: str, display: np.array) -> np.array:
+    tokens = line.split(" ")
+    shift = int(tokens[-1])
+    row = int(tokens[-3].split("=")[-1])
+    display[row] = np.roll(display[row], shift)
+    return display
+
+
+def rotate_column(line: str, display: np.array) -> np.array:
+    tokens = line.split(" ")
+    shift = int(tokens[-1])
+    col = int(tokens[-3].split("=")[-1])
+    display[:, col] = np.roll(display[:, col], shift, )
+    return display
+
+
+def parse_command(line: str):
+    if "rotate row" in line:
+        return rotate_row
+    elif "rotate column" in line:
+        return rotate_column
+    elif "rect" in line:
+        return rect
+    else:
+        raise Exception("Unknown command")
+
+
+def get_display():
+    return np.zeros(shape=(6, 50))
+
+disp = np.zeros(shape=(3, 7))
+for line in example:
+    func = parse_command(line)
+    func(line, disp)
+    print(disp)
+    print()
+
+disp = get_display()
+for line in data:
+    func = parse_command(line)
+    func(line, disp)
+print(disp)
+print(np.count_nonzero(disp))
